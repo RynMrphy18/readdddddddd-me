eb8f2db5079036c1c319d03d2d740d8e14428ed1
Ryan <rmurph1800@gmail.com>
2021-12-06 13:22:16 -0500

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,131 @@ const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true
+            } else {
+                console.log('You need to enter a title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your Github username? (Required)',
+        validate: ghInput => {
+            if (ghInput) {
+                return true
+            } else {
+                console.log('You need to enter a github username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (Required)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true
+            } else {
+                console.log('You need to enter an email!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project (Required)',
+        validate: installInput => {
+            if (installInput) {
+                return true
+            } else {
+                console.log('You need to explain how to install your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project? (Required)',
+        validate: installInput => {
+            if (installInput) {
+                return true
+            } else {
+                console.log('You need to explain how to install your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project? (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true
+            } else {
+                console.log('You need to explain how to use your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license are you using?',
+        choices: ['agpl', 'apache', 'mit', 'no license']
+    },
+    {
+        type: 'confirm',
+        name: 'confirmContrib',
+        message: 'Can other developers contribute to your project? (Required)',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'How should others contribute? (Required)',
+        when: ({ confirmContrib }) => {
+            if (confirmContrib) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: contributorInput=> {
+            if(contributorInput) {
+                return true;
+            } else {
+                console.log('You need to explain how others should contribute!');
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How should your project be tested? (Required)',
+        validate: testInput => {
+            if (testInput) {
+                return true
+            } else {
+                console.log('You need to explain how to test your project!');
+                return false;
+            }
+        }
+    },
+];
 
 
 // TODO: Create a function to write README file
